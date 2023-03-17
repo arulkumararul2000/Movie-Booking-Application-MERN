34ae9c1b38133dc60b6786fee436efdfcf1ee261
arulkumararul <arulkumararul2000@gmail.com>
2023-03-17 20:50:43 +0530

    initial commit

--- movies/src/components/Movies/Movies.js
+++ movies/src/components/Movies/Movies.js
@@ -13,7 +13,7 @@ const Movies = () => {
  
   return (
     <Box margin={"auto"} marginTop={4}>
-  <Typography variant='h4' padding={2} textAlign='center' bgcolor={'#900C3F'} width="40%" color="white" margin={"auto"}>
+  <Typography variant='h4' padding={2} borderRadius={5} textAlign='center' bgcolor={'#900C3F'} width="40%" color="white" margin={"auto"}>
     All Movies
   </Typography>
   <Box width={"100%"} margin={'auto'} marginTop='5' display={"flex"} justifyContent='flex-start' flexWrap={'wrap'}>
